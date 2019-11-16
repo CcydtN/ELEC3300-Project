9625c34154b28209eea52ae19ce92262a722d9f0
pynnek <51289140+pynnek@users.noreply.github.com>
2019-11-16 19:39:51 +0800

    Remove TestFile

--- TestFile/wav_read.m
+++
@@ -1,8 +0,0 @@
-[x,fs] = audioread("A4_Tone.wav",'native');
-x = x';
-t = 0:1/fs:2/440-1/fs;
-x = typecast(x,'uint16');
-x = bitxor(x,uint16(ones(1,length(x))*2^(15)));
-l = length(t);
-
-plot(t,x(1,1:l));
